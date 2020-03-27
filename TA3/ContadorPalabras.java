4ea03614b70449ba39db4bd7c60257ca7900ee6c
SebaColina <sebacoli00@gmail.com>
2020-03-26 23:28:40 -0300

    cambios documentacion

--- TA3/ContadorPalabras.java
+++ TA3/ContadorPalabras.java
@@ -1,22 +1,8 @@
 package TA3;
 
 import java.io.File;
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.*;
-/*
-Ejercicio #1 – Contador de palabras
-Es necesario implementar un programa que sea capaz de contar la cantidad de palabras de una frase
-(las palabras son separadas por un espacio). Para ello es necesario crear una clase Java (llamada “ContadorPalabras”)
-capaz de realizar esta funcionalidad y otra clase Java (llamada “Principal”) para realizar las pruebas
-(a través del método “main”).
-NOTAS IMPORTANTES:
-•	No se puede utilizar la función “split” para resolver este problema
-•	Una palabra debe contener al menos un carácter alfabético
-•	Se puede usar métodos como “isLetter”, “isDigit”, etc….
-*/
 
 /*
     Ejercicio 1: Lenguaje natural
@@ -25,10 +11,19 @@ NOTAS IMPORTANTES:
     por lo tanto finaliza una palabra y se incrementa el contador de palabras en 1.
 */
 public class ContadorPalabras {
-    int consonantes = 0;
-    int vocales = 0;
+    private int consonantes = 0;
+    private int vocales = 0;
 
-    public int cuentaPalabras(String texto) {
+    public int getVocales() {
+        return this.vocales;
+    }
+
+    public int getConsonantes() {
+        return this.consonantes;
+    }
+
+    // cuenta las palabras de una string recibida por parametros
+    public int cuentaPalabrasString(String texto) {
         if (texto == null || texto.isEmpty()) {
             return 0;
         }
@@ -61,9 +56,12 @@ public class ContadorPalabras {
      * contador "vocales", en caso contrario comparamos si es letra incrementamos el
      * contador "consonante" luego asignamos el valor de los contadores a las
      * variables de instancia para luego consutarlas mediante los getters.
+     *
+     * COMENTARIOS DEL SUBEQUIPO B
+     * hemos renombrado el nombre del metodo ya que debe comenzar con minuscula y ademas es recomendable que los nombres
+     * sean verbos
      */
-
-    public void CantVocalesYConsonantes(String frase) {
+    public void contarVocalesYConsonantes(String frase) {
         int vocales = 0;
         int consonantes = 0;
         for (int i = 0; i < frase.length(); i++) {
@@ -79,23 +77,18 @@ public class ContadorPalabras {
         this.consonantes = consonantes;
     }
 
-    public int getVocales() {
-        return this.vocales;
-    }
-
-    public int getConsonantes() {
-        return this.consonantes;
-    }
-
     /*
      * Ej 2. Sub-equipo B. Lenguaje natural. Este metodo recorre un texto recibido
      * por parametro y verifica cuantas palabras poseen una cantidad mayor de
      * caracteres especificadas por parametro. Este devuelve como resultado la
      * cantidad de palabras que tienen una mayor cantidad de caracteres a los
      * especificados.
+     *
+     * COMENTARIOS DEL SUBEQUIPO A
+     * hemos renombrado el nombre del metodo ya que debe comenzar con minuscula y ademas es recomendable que los nombres
+     * sean verbos
      */
-
-    public int CantPalabras(String texto, int caracterMayor) {
+    public int contarPalabrasMayores(String texto, int caracterMayor) {
         if (texto == null || texto.isEmpty()) {
             return 0;
         }
@@ -129,30 +122,9 @@ public class ContadorPalabras {
     }
 
     // ejercicio 3
-    /*
-     * Referencias: tutoriales y material “Manejo básico de lectura / escritura de
-     * archivos” publicados. Se solicita extender las funcionalidades del Contador
-     * de Palabras desarrollado en la Unidad anterior. Las nuevas funcionalidades
-     * son: ● Retornar un vector de Strings con las líneas de texto contenidas en un
-     * archivo dado, e indicar la cantidad de líneas leídas. ● Dado un vector de
-     * Strings que contiene las líneas de texto, retornar la cantidad de palabras
-     * contenidas. Firmas que deben tener los métodos solicitados class
-     * ContadorPalabras { … public String[] obtenerLineas (String archivo); public
-     * int cantPalabras (String[] lineasArchivo); } A realizar : 1. Describir en
-     * lenguaje natural las nuevas funcionalidades. Escribir qué métodos de manejo
-     * de archivo estándar de JAVA se van a utilizar. 2. Implementar las
-     * funcionalidades solicitadas, modificando la clase “ContadorPalabras”
-     * anteriormente desarrollada. ¡Revisar la documentación de la API de JAVA para
-     * las clases y métodos utilizados! 3. Desarrollar una clase “Principal” capaz
-     * de probar las nuevas funcionalidades a través del método “main”. En este
-     * método, se ha de instanciar un ContadorPalabras y emitir por consola la
-     * cantidad de palabras de un archivo cuyo nombre se pasa por parámetro de línea
-     * de comando (revisar la documentación del método main). Se proveerá un archivo
-     * de texto de ejemplo en la webasignatura.
-     */
-
-    // Este metodo recibe un archivo y devuelve un array capaz de almacenar todas las lineas del mismo.
-    public String[] ObtenerLineas(String txt) {
+    // Este metodo recibe un archivo y devuelve un array capaz de almacenar todas
+    // las lineas del mismo.
+    public String[] ObtenerLineasDeArchivo(String txt) {
         int contadorLineas = 0;
 
         try {
@@ -181,37 +153,38 @@ public class ContadorPalabras {
 
         } catch (Exception e) {
             e.printStackTrace();
-            String [] array = new String[0];
+            String[] array = new String[0];
             return array;
         }
     }
 
     /*
-    * metodo que cuenta la cantidad de palabras que hay en un array
-    */
+     * metodo que cuenta la cantidad de palabras que hay en un array
+     */
     public int CantidadPalabras(String[] lineasArchivo) {
         ContadorPalabras contadorPalabras = new ContadorPalabras();
         int contador = 0;
         for (int i = 0; i < lineasArchivo.length; i++) {
-            contador += contadorPalabras.cuentaPalabras(lineasArchivo[i]);
+            contador += contadorPalabras.cuentaPalabrasString(lineasArchivo[i]);
         }
         return contador;
 
     }
+}
 
+// clase que contiene el metodo main en el cual estaremos testeando nuestro programa con cada uno de sus metodos.
+class Principal {
     public static void main(String[] args) {
         ContadorPalabras prueba = new ContadorPalabras();
-        System.out.println(("Cantidad de palabras con largo mayor al indicado: " + prueba.CantPalabras("hola hola hola hol", 3)));
+        System.out.println(("Cantidad de palabras con largo mayor al indicado: "
+                + prueba.contarPalabrasMayores("hola hola hola hol", 3)));
 
-        prueba.CantVocalesYConsonantes("hola hola hOla hol123");
+        prueba.contarVocalesYConsonantes("hola hola hOla hol123");
         System.out.println("Cantidad De Consonantes: " + prueba.getConsonantes());
         System.out.println("Cantidad De Vocales: " + prueba.getVocales());
 
-
-        //Ejercicio 3
-       // System.out.println("Lineas del archivo: "+prueba.ObtenerLineas("C:/Users/FIT/Desktop/UCU/Algoritmos I/RepoEjerciciosAplicacion/TrabajosAplicacion"));
-
-        System.out.println("Cantidad de palabras: "+ prueba.CantidadPalabras(prueba.ObtenerLineas("C:/Users/FIT/Desktop/UCU/Algoritmos I/RepoEjerciciosAplicacion/TrabajosAplicacion/TA3/archivoPrueba.txt")));
+        System.out.println("Cantidad de palabras: " + prueba.CantidadPalabras(prueba.ObtenerLineasDeArchivo(
+                "C:/Users/FIT/Desktop/UCU/Algoritmos I/RepoEjerciciosAplicacion/TrabajosAplicacion/TA3/archivoPrueba.txt")));
 
     }
 }
